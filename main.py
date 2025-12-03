eb1c7a60e247b6c9dea658c408b67f4a5c882524
Chintan Patel <chintanpatel02407@gmail.com>
2025-12-03 11:48:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-
+import sys
+import colorama
+import time
+import argparse
+import json
+import httpx
+import hmac
+import hashlib
+import urllib
+import requests
+from httpx import get
+from bs4 import BeautifulSoup
+from colorama import Fore, Back, Style, init
