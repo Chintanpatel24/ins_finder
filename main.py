4409c43a49c8ff59060e9f1a6f85bacff3f38bb1
Chintan Patel <chintanpatel02407@gmail.com>
2025-12-03 11:50:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,16 @@ import requests
 from httpx import get
 from bs4 import BeautifulSoup
 from colorama import Fore, Back, Style, init
+
+
+colorama.init(autoreset=True)
+
+
+def banner():
+    print("                _ _   _                ")
+    print("  _  _ ___ ___ (_) |_( )___  _ __  ___ ")
+    print(" | || / -_|_-< | |  _|/(_-< | '  \/ -_)")
+    print("  \_, \___/__/ |_|\__| /__/ |_|_|_\___|")
+    print("  |__/                                 ")
+    print("\n\tTwitter: " + Fore.MAGENTA + "@blackeko5")
+
